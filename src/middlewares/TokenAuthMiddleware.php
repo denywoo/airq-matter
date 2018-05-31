8f2ad84c47f756e608a0278064a3559428b570d0
Denis Maltsev <denywoo@gmail.com>
2018-05-31 03:35:06 +0300

    Added auth by token with Authorization header or with token given in request body

--- src/middlewares/TokenAuthMiddleware.php
+++ src/middlewares/TokenAuthMiddleware.php
@@ -1,39 +1,54 @@
 <?php
 namespace app\middlewares;
 
+use Slim\Http\Response;
 use Psr\Log\LoggerInterface;
+use Slim\Http\Request;
 
 class TokenAuthMiddleware
 {
-    private $_tokens = [];
-    private $_logger = null;
+    private $_tokens;
+    private $_logger;
+    private $_tokenParamName;
 
     /**
      * TokenAuthMiddleware constructor.
      * @param string[] $tokens
      * @param LoggerInterface|null $logger
+     * @param string $tokenParamName
      */
-    public function __construct(array $tokens, LoggerInterface $logger = null)
+    public function __construct(array $tokens, LoggerInterface $logger = null, string $tokenParamName = 'token')
     {
         $this->_tokens = $tokens;
         $this->_logger = $logger;
+        $this->_tokenParamName = $tokenParamName;
     }
 
     /**
      * middleware invokable class
      *
-     * @param  \Psr\Http\Message\ServerRequestInterface $request PSR7 request
-     * @param  \Psr\Http\Message\ResponseInterface $response PSR7 response
+     * @param  Request $request PSR7 request
+     * @param  Response $response PSR7 response
      * @param  callable $next Next middleware
      *
      * @return \Psr\Http\Message\ResponseInterface
      */
     public function __invoke($request, $response, $next)
     {
-        $queryParams = $request->getQueryParams();
-        $token = $queryParams['token'];
-        if (!in_array($token, $this->_tokens)) {
-            $this->logError("Access denied incorrect token", $queryParams);
+        $token = null;
+
+        if ($request->hasHeader('Authorization')) {
+            $authHeader = $request->getHeaderLine('Authorization');
+            $isMatch = preg_match('/Token\s+(?<token>.*)/', $authHeader, $matches);
+            if ($isMatch && array_key_exists('token', $matches)) {
+                $token = $matches['token'];
+            }
+        } else {
+            $token = $request->getParam($this->_tokenParamName);
+        }
+
+        if (empty($token) || !in_array($token, $this->_tokens)) {
+            $this->logError("Access denied: incorrect token");
             return $response->withStatus(403);
         }
 

dc7c1fb9e7e8cd387caaa58ce936f306ef818503
Denis Maltsev <denywoo@gmail.com>
2018-06-03 00:23:12 +0300

    Apply auth to whole app

--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,8 @@
 <?php
-
 // Routes
 
 /** @var \Slim\App $app */
 
-$app->get('/publish/{service}', \app\actions\PublishDataAction::class)->add($app->getContainer()->get('auth'));
+$app->get('/publish/{service}', \app\actions\PublishDataAction::class);
 
-$app->post('/mattermost', \app\actions\MattermostAirqAction::class)->add($app->getContainer()->get('auth'));
+$app->post('/mattermost', \app\actions\MattermostAirqAction::class);
